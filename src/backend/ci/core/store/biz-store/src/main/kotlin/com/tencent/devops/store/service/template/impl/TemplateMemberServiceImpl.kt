4f59b71cba99d0ada9443f99f62a97aaf99cbf73
Mr-SakuraManJi <1516908003@qq.com>
2023-02-22 17:57:47 +0800

    【流水线模板】支持成员管理 #1833

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/template/impl/TemplateMemberServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/template/impl/TemplateMemberServiceImpl.kt
@@ -27,8 +27,14 @@
 
 package com.tencent.devops.store.service.template.impl
 
+import com.tencent.devops.common.api.constant.CommonMessageCode
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.service.utils.MessageCodeUtil
 import com.tencent.devops.store.dao.template.MarketTemplateDao
+import com.tencent.devops.store.pojo.common.StoreMemberReq
+import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
 import com.tencent.devops.store.service.common.impl.StoreMemberServiceImpl
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -38,7 +44,72 @@ class TemplateMemberServiceImpl : StoreMemberServiceImpl() {
     @Autowired
     private lateinit var marketTemplateDao: MarketTemplateDao
 
+    private val logger = LoggerFactory.getLogger(TemplateMemberServiceImpl::class.java)
+
     override fun getStoreName(storeCode: String): String {
         return marketTemplateDao.getLatestTemplateByCode(dslContext, storeCode)?.templateName ?: ""
+    }
+
+    /**
+     * 添加模板成员
+     */
+    override fun add(
+        userId: String,
+        storeMemberReq: StoreMemberReq,
+        storeType: StoreTypeEnum,
+        collaborationFlag: Boolean?,
+        sendNotify: Boolean,
+        checkPermissionFlag: Boolean,
+        testProjectCode: String?
+    ): Result<Boolean> {
+        logger.info("addTemplateMember params:$userId|$storeMemberReq|$storeType|$collaborationFlag|$sendNotify")
+        val templateCode = storeMemberReq.storeCode
+        if (checkPermissionFlag && !storeMemberDao.isStoreAdmin(
+                dslContext = dslContext,
+                userId = userId,
+                storeCode = templateCode,
+                storeType = storeType.type.toByte()
+            )
+        ) {
+            return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.PERMISSION_DENIED)
+        }
+        return super.add(
+            userId = userId,
+            storeMemberReq = storeMemberReq,
+            storeType = storeType,
+            collaborationFlag = collaborationFlag,
+            sendNotify = sendNotify,
+            checkPermissionFlag = checkPermissionFlag,
+            testProjectCode = testProjectCode
+        )
+    }
+
+    /**
+     * 删除模板成员
+     */
+    override fun delete(
+        userId: String,
+        id: String,
+        storeCode: String,
+        storeType: StoreTypeEnum,
+        checkPermissionFlag: Boolean
+    ): Result<Boolean> {
+        logger.info("deleteTemplateMember params:[$userId|$id|$storeCode|$storeType|$checkPermissionFlag]")
+        val memberRecord = storeMemberDao.getById(dslContext, id)
+            ?: return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.PARAMETER_IS_INVALID, arrayOf(id))
+        // 如果删除的是管理员，只剩一个管理员则不允许删除
+        if ((memberRecord.type).toInt() == 0) {
+            val validateAdminResult = isStoreHasAdmins(storeCode, storeType)
+            if (validateAdminResult.isNotOk()) {
+                return Result(status = validateAdminResult.status, message = validateAdminResult.message, data = false)
+            }
+        }
+        return super.delete(
+            userId = userId,
+            id = id,
+            storeCode = storeCode,
+            storeType = storeType,
+            checkPermissionFlag = checkPermissionFlag
+        )
     }
 }
